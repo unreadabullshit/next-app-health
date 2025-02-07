b66ef188674ee372232d7ec4457f8c2767593091
Hector Araujo <contact@voit.dev>
2025-02-07 10:44:40 -0300

    feat: add dropzone component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-import {ModeToggle} from '@/components/dark-mode-switch'
-import Image from "next/image";
+'use client'
+
+import { useState } from 'react';
+import {
+  Dropzone,
+  DropzoneContent,
+  DropzoneEmptyState,
+} from '@/components/ui/kibo-ui/dropzone';
 
 export default function Home() {
+  const [files, setFiles] = useState<File[] | undefined>();
+
+  const handleDrop = (files: File[]) => {
+    console.log(files);
+    setFiles(files);
+  };
+  
   return (
     <div>
-      <h1>Hell o'World</h1>
-      <ModeToggle/>
+      <div className="flex h-screen w-full items-center justify-center bg-secondary p-8">
+      <Dropzone
+        onDrop={handleDrop}
+        src={files}
+        multiple
+        maxFiles={Infinity}
+        onError={console.error}
+      >
+        <DropzoneEmptyState />
+        <DropzoneContent />
+      </Dropzone>
+    </div>
       </div>
   );
 }
