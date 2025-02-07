c48145de32daa2f7f03c0ee2f2fe1d6672669671
Hector Araujo <contact@voit.dev>
2025-02-07 01:44:44 -0300

    feat: add dark mode(just because)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import {ModeToggle} from '@/components/dark-mode-switch'
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div><h1>Hell o'World</h1></div>
+    <div>
+      <h1>Hell o'World</h1>
+      <ModeToggle/>
+      </div>
   );
 }
